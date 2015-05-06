62f454575d8742859b644c5d15a64c16e378bc52
Jay D <jayfdermody@gmail.com>
2015-05-06 05:12:40 +0000

    make into postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150428063019) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pies", force: true do |t|
     t.string   "identifier"
     t.text     "notes"
