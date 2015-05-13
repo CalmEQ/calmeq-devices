2e1bf192512f18087290607da0438d80df1fa79b
Jay D <jayfdermody@gmail.com>
2015-05-13 07:15:05 +0000

    add more logging hopefully

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Add loging with logentries for heduko
+Rails.logger = Le.new('fb07f06c-69f9-4a5e-8269-443bb1188dc9', :debug => true, :local => true)
