14ac6b47a12d0cb6b53eccfda54bd5e8d808237c
cprohan <cprohan@gmail.com>
2015-06-10 22:49:19 +0000

    adding lookup by mac id

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -7,18 +7,19 @@ class ReadingsController < ApplicationController
 
   def show
     @reading = Reading.find(params[:id])
-    @py = Py.find(@reading.py_id)
+    @py = Py.find(@reading.identifier)
   end
   
   def create
-    @py = Py.find(params[:py_id])
-    @newreading = @py.readings.create(reading_params)
-    redirect_to py_reading_path(@py, @newreading)
+    @py = Py.find_or_create_by(identifier: params[:identifier])
+    @reading = @py.readings.create(reading_params)
+    @reading.py_id = @py.id
+    redirect_to py_reading_path(@py, @reading)
   end
  
   private
     def reading_params
-      params.require(:reading).permit(:lat, :lon, :dblvl, :devicetime, :py_id)
+      params.require(:reading).permit(:lat, :lon, :dblvl, :devicetime, :py_id, :identifier)
     end
 
 end
