7a1f7d30416c9c61e6f685b1d070d460e6e0285f
Jay D <jayfdermody@gmail.com>
2015-06-22 05:08:34 +0000

    bugfixin

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,3 +1,13 @@
+# Step #1
+# working test suite
+
+# Step #2
+# Get Gemnasium
+sudo sh -c 'echo "deb http://apt.gemnasium.com stable main" > /etc/apt/sources.list.d/gemnasium.list'
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com E5CEAB0AC5F1CA2A
+sudo apt-get update
+sudo apt-get install gemnasium-toolbelt
+
 # Step #3 
 # Specify the command and/or script to execute to run the tests into the environment variable GEMNASIUM_TESTSUITE. Ie:
 export GEMNASIUM_TESTSUITE="bundle exec rake test:functionals test:units"
